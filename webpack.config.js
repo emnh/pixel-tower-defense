d10ea2500e3f1f1ffdf39cd9bb8ca65a5dbf9123
Eivind Magnus Hvidevold <hvidevold@gmail.com>
2020-01-17 11:07:23 +0100

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  //mode: 'production',
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'index.js'
+  },
+  devServer: {
+    index: 'default.html',
+    contentBase: path.join(__dirname, "."),
+    compress: true,
+    port: 8080
+  },
+  plugins: [
+  ]
+};
