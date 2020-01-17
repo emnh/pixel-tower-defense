f6248d86c5de882340a67730061ca994816d4043
Eivind Magnus Hvidevold <hvidevold@gmail.com>
2020-01-17 12:50:59 +0100

    random terrain established

--- src/index.js
+++ src/index.js
@@ -36,19 +36,114 @@ renderer.setSize(WIDTH, HEIGHT);
 // DOM element.
 container.appendChild(renderer.domElement);
 
+// Set up the plane vars
+const SIZE = 128;
+
+const XSEGMENTS = 64;
+const YSEGMENTS = 64;
+
+const tileSize = 32;
+
+const textureWidth = 2048;
+const textureHeight = 3040;
+
+const xf = tileSize / textureWidth;
+const yf = tileSize / textureHeight;
+const xc = 14 * xf;
+const yc = 10 * yf;
+
+const tilesX = 64;
+const tilesY = 95;
+
+// Create a buffer with color data
+const dataTextureSize = XSEGMENTS * YSEGMENTS;
+const data = new Float32Array(4 * dataTextureSize);
+
+for (let i = 0; i < dataTextureSize; i++) {
+	const stride = i * 4;
+	data[stride] = Math.floor(Math.random() * tilesX) * xf;
+	data[stride + 1] = (2 + Math.floor(Math.random() * 8)) * yf;
+	data[stride + 2] = 0;
+	data[stride + 3] = 0;
+}
+
+// used the buffer to create a DataTexture
+const dataTexture = new THREE.DataTexture(data, XSEGMENTS, YSEGMENTS, THREE.RGBAFormat, THREE.FloatType);
+
+const texture = new THREE.TextureLoader().load('art/images/ProjectUtumno_full.png' );
+
+/*
+texture.wrapS = THREE.RepeatWrapping;
+texture.wrapT = THREE.RepeatWrapping;
+texture.offset.x = xc;
+texture.offset.y = yc;
+texture.repeat.x = tileSize / textureWidth;
+texture.repeat.y = tileSize / textureHeight;
+*/
+texture.minFilter = THREE.NearestFilter;
+texture.magFilter = THREE.NearestFilter;
+texture.anisotropy = renderer.capabilities.getMaxAnisotropy();
 
 // create the plane's material
+/*
 const planeMaterial =
   new THREE.MeshLambertMaterial(
     {
-      color: 0xCC0000
+      map: texture
     });
+*/
+const vertexShader = `
+  varying vec2 vUv;
 
+  void main()
+  {
+    vUv = uv;
+    vec4 mvPosition = modelViewMatrix * vec4(position, 1.0);
+    gl_Position = projectionMatrix * mvPosition;
+  }
+`;
 
-// Set up the plane vars
-const SIZE = 100;
-const XSEGMENTS = 16;
-const YSEGMENTS = 16;
+const fragmentShader = `
+  uniform float time;
+  uniform vec2 tileSizeOnTexture;
+  uniform vec2 tileSizeOnPlane;
+  uniform vec2 segments;
+  uniform sampler2D mapIndex;
+  uniform sampler2D map;
+
+  varying vec2 vUv;
+
+  void main( void ) {
+
+    vec2 uvIndex = floor(vUv * segments) / segments;
+
+    vec2 uvRel = (vUv - uvIndex) * tileSizeOnPlane;
+
+    vec2 index = texture2D(mapIndex, uvIndex).xy;
+    index.y = 1.0 - index.y;
+
+    vec4 color = texture2D(map, index + uvRel);
+
+    gl_FragColor = color;
+
+  }
+`;
+
+const planeMaterial =
+  new THREE.ShaderMaterial(
+    {
+      uniforms: {
+        time: { value: 1.0 },
+        resolution: { value: new THREE.Vector2() },
+        segments: { value: new THREE.Vector2(XSEGMENTS, YSEGMENTS) },
+        tileSizeOnTexture: { value: new THREE.Vector2(tileSize / textureWidth, tileSize / textureHeight) },
+        tileSizeOnPlane: { value: new THREE.Vector2(XSEGMENTS / SIZE, YSEGMENTS / SIZE) },
+        map: { value: texture },
+        mapIndex: { value: dataTexture }
+      },
+      vertexShader: vertexShader,
+      fragmentShader: fragmentShader
+    });
 
 // Create a new mesh with
 // plane geometry - we will cover
