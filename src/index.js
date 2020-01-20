44264170910401ddb5a7054c55e71a00f7731f2b
Eivind Magnus Hvidevold <hvidevold@gmail.com>
2020-01-20 16:36:44 +0100

    dirLight. terrain. barge.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
 const main = function() {
   const THREE = require('three');
   const SimplexNoise = require('simplex-noise');
+  const seedrandom = require('seedrandom');
   const unitShader = require('./unitShader.js');
+  const bargeJSON = require('./barge.js').bargeJSON;
+
+  // Global PRNG: set Math.random.
+  seedrandom('hello.', { global: true });
 
   // Set the scene size.
   const WIDTH = window.innerWidth;
@@ -252,24 +257,60 @@ const main = function() {
   };
 
   const sets = [
-    ["5X23","22X34","6X27","6X0","7X32","8X34"],  // sandstone
-    ["2X20","5X41","4X12","5X22","2X56","5X46"],  // dark
-    ["8X58","4X24","2X3","3X28","6X0","4X26"],    // acidic 2
-    ["5X15","22X55","7X32","4X38","4X50","6X29"], // arctic
-    ["5X61","6X26","22X15","2X37","9X40","8X34"], // green
-    ["6X11","3X19","2X36","6X29","8X49","21X21"], // cobble
-    ["21X47","6X60","22X58","2X8","6X31","22X37"],// oceanic
-    ["22X15","6X1","2X37","2X38","5X40","4X39"],  // green desert
-    ["22X9","9X23","4X9","9X1","9X2","9X35"],     // marble
-    ["23X7","7X42","7X32","3X62","8X37","5X35"],  // desert
-    ["21X47","9X16","9X11","8X52","3X44","2X2"],  // acidic
-    ["22X35","8X25","2X57","2X7","4X12","6X41"],  // darkgreen
-    ["21X21","8X25","4X11","6X45","6X41","2X26"], // lightgreen
-    ["22X37","2X9","3X61","5X47","5X37","6X19"],  // contrast
-    ["23X7","4X23","3X62","8X43","6X2","2X27"],   // limestone
-    ["22X37","3X20","5X40","9X23","5X13","5X48"]  // grass+dirt
+    ["5X23","22X34","6X27","6X0","7X32","8X34"],      // sandstone
+    ["2X20","5X41","4X12","5X22","2X56","5X46"],      // dark
+    ["8X58","4X24","2X3","3X28","6X0","4X26"],        // acidic 2
+    ["5X15","22X55","7X32","4X38","4X50","6X29"],     // arctic
+    ["5X61","6X26","22X15","2X37","9X40","8X34"],     // green
+    ["6X11","3X19","2X36","6X29","8X49","21X21"],     // cobble
+    ["21X47","6X60","22X58","2X8","6X31","22X37"],    // oceanic
+    ["22X15","6X1","2X37","2X38","5X40","4X39"],      // green desert
+    ["22X9","9X23","4X9","9X1","9X2","9X35"],         // marble
+    ["23X7","7X42","7X32","3X62","8X37","5X35"],      // desert
+    ["21X47","9X16","9X11","8X52","3X44","2X2"],      // acidic
+    ["22X35","8X25","2X57","2X7","4X12","6X41"],      // darkgreen
+    ["21X21","8X25","4X11","6X45","6X41","2X26"],     // lightgreen
+    ["22X37","2X9","3X61","5X47","5X37","6X19"],      // contrast
+    ["23X7","4X23","3X62","8X43","6X2","2X27"],       // limestone
+    ["22X37","3X20","5X40","9X23","5X13","5X48"],     // grass+dirt
+    ["23X2","9X11","9X14","9X15","7X21","14X41"]      // standard
   ];
 
+  const heightMap =
+    [
+[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+[35,51,56,61,74,83,90,90,90,90,90,99,111,116,116,116,116,116,116,116,116,116,114,104,80,0,0,0,0,0,0,0],
+[87,103,108,111,111,113,113,113,113,113,113,115,121,123,123,123,123,123,123,123,123,123,123,120,89,0,0,0,0,0,0,0],
+[98,108,114,115,115,113,113,113,113,113,113,115,115,116,116,116,116,116,116,116,116,116,116,111,89,0,0,0,0,0,0,0],
+[180,169,161,150,157,166,176,176,176,176,176,185,194,200,200,200,200,200,200,200,200,200,194,173,136,0,0,0,0,0,0,0],
+[244,253,253,252,252,252,252,252,252,252,252,252,248,248,248,249,251,251,251,248,248,248,248,240,188,0,0,0,0,0,0,0],
+[230,244,245,244,244,244,244,244,244,244,244,244,248,248,248,249,252,254,251,248,248,248,248,240,188,0,0,0,0,0,0,0],
+[120,138,138,136,136,136,136,136,136,136,136,158,173,184,189,201,230,230,225,200,200,194,189,169,136,0,0,0,0,0,0,0],
+[80,110,117,118,117,117,117,117,117,117,117,117,117,117,117,116,116,116,116,116,116,116,117,111,89,0,0,0,0,0,0,0],
+[80,117,122,122,121,121,121,121,121,121,121,121,121,121,121,123,123,123,123,123,123,123,121,120,89,0,0,0,0,0,0,0],
+[71,100,110,111,110,110,110,110,110,110,110,110,110,110,112,114,116,116,116,116,116,114,112,100,80,0,0,0,0,0,0,0],
+[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+[0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+[0,0,0,0,0,0,89,111,119,117,117,117,116,116,114,112,112,114,116,116,116,116,116,116,116,116,116,116,116,116,108,89],
+[0,0,0,0,0,0,147,130,134,128,128,128,123,123,123,128,128,123,123,123,123,123,123,123,123,123,123,123,123,123,123,120],
+[0,0,0,0,0,0,187,195,172,172,163,158,153,148,148,144,144,148,148,148,148,148,148,148,148,148,148,148,148,148,166,188],
+[0,0,0,0,0,0,218,254,255,254,254,254,254,254,245,237,237,246,255,255,255,255,254,254,255,255,255,255,255,255,255,250],
+[0,0,0,0,0,0,180,219,232,226,217,217,212,207,200,200,200,220,224,235,227,227,216,225,231,237,240,243,245,244,240,218],
+[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+[76,96,106,109,111,109,107,105,105,105,105,102,97,91,91,91,91,91,91,98,98,104,105,105,91,60,0,0,0,0,0,0],
+[91,112,116,118,118,118,116,116,116,116,116,116,114,114,114,114,114,114,114,116,117,116,117,117,112,85,0,0,0,0,0,0],
+[86,106,111,111,111,111,112,112,112,112,112,112,114,114,114,114,114,114,114,116,117,116,116,116,112,85,0,0,0,0,0,0],
+[166,197,212,222,230,222,215,208,208,208,201,183,176,168,168,168,176,176,174,160,166,168,183,183,188,174,0,0,0,0,0,0],
+[202,244,252,252,252,252,252,252,252,252,252,248,251,253,251,250,253,253,250,252,252,255,255,255,255,188,0,0,0,0,0,0],
+[190,230,241,244,244,244,244,244,244,244,244,248,252,253,251,250,251,252,250,244,244,241,241,241,225,188,0,0,0,0,0,0]
+];
 
   /*
   for (let i = 11; i < 39; i++) {
@@ -305,9 +346,10 @@ const main = function() {
   const groundKeys = Object.keys(uniqueGround);
   const tset = [];
   const choose = k => k[Math.floor(Math.random() * k.length)];
-  const chosenSet = choose(sets);
+  //const chosenSet = choose(sets);
+  const chosenSet = sets[16];
   for (let i = 0; i < 6; i++) {
-    const key = 
+    const key =
       randomTerrain ?
         (i === 0 ?
           choose(waterKeys) :
@@ -330,11 +372,21 @@ const main = function() {
   }
   */
 
-  const sf = function(x, y) {
+  const sf2 = function(x, y) {
     const scale = 2.0 * 1.0 / XSEGMENTS;
     const ret = (simplex.noise2D(x * scale, y * scale) + 1.0) / 2.0;
     return ret;
   };
+
+  const sf = function(x, y) {
+    const scale = 1.0 / (XSEGMENTS / XSEGMENTRES);
+    const xmax = XSEGMENTS / XSEGMENTRES - 1;
+    const xs = Math.min(xmax, Math.floor(x * scale));
+    const ys = Math.min(xmax, Math.floor(y * scale));
+    const ret = heightMap[ys][xs] / 256.0;
+    const ret2 = sf2(x, y);
+    return ret * 0.7 + ret2 * 0.3;
+  }
 
   const simplex = new SimplexNoise();
   for (let i = 0; i < dataTextureSize; i++) {
@@ -364,7 +416,7 @@ const main = function() {
       data[stride + 3] = 1.0;
     }
   }
-  
+
   for (let i = 0; i < dataTextureSizeUnits; i++) {
     const stride = i * 4;
     dataUnits[stride] = Math.floor(Math.random() * tilesX) * xf;
@@ -574,6 +626,7 @@ const main = function() {
       const float lightSpeed = 2.0;
       const float lightCount = 16.0;
       vec4 lcolor = vec4(vec3(0.0), color.a);
+      // TODO: uniform
       float dirLight = max(0.0, (dot(vNormal, normalize(vec3(-0.65, 1.0, -0.65)))));
       //dirLight *= (0.5 + ((sin(vPosition.x) + 1.0) / 2.0 + (sin(vPosition.z) + 1.0) / 2.0) / 2.0);
       //dirLight /= pow(length(vPosition), 2.0) * 0.2;
@@ -851,7 +904,7 @@ const main = function() {
   //const units = new THREE.Mesh(geo2, new THREE.MeshStandardMaterial());
   const units = new THREE.Mesh(geo2, unitMaterial);
 
-  scene.add(units);
+  //scene.add(units);
 
 
   camera.lookAt(new THREE.Vector3(plane.position.x, plane.position.y - 2, plane.position.z));
@@ -859,17 +912,27 @@ const main = function() {
   const ambient = new THREE.AmbientLight(0xFFFFFF);
   //scene.add(ambient);
 
-  // create a point light
-  const pointLight =
-    new THREE.PointLight(0xFFFFFF);
+  const dirLight = new THREE.DirectionalLight(0xFFFFFF);
 
-  // set its position
-  pointLight.position.x = 0;
-  pointLight.position.y = 0;
-  pointLight.position.z = 0;
+  dirLight.position.set(-0.65, 1.0, -0.65);
+  dirLight.lookAt(scene.position);
 
-  // add to the scene
-  scene.add(pointLight);
+  scene.add(dirLight);
+
+  const loader = new THREE.ObjectLoader();
+
+  const bargeProto = loader.parse(bargeJSON);
+
+  for (let i = 0; i < 10; i++) {
+    const barge = bargeProto.clone();
+    const bsc = 0.2;
+    barge.scale.set(bsc, bsc, bsc);
+    barge.position.x = -15.0 + 3.0 * i;
+    barge.position.y = 3.0;
+    barge.position.z = -15.0;
+    barge.rotation.y = Math.PI / 2.0;
+    plane.add(barge);
+  }
 
   // Draw!
   renderer.render(scene, camera);
@@ -896,5 +959,6 @@ const main = function() {
 try {
   main();
 } catch (error) {
+  throw(error);
   document.body.innerHTML = '<pre>' + error.stack + '</pre>';
 }
