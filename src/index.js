2e4f0ce48b3619aedabb746459523ac04bc07adf
Eivind Magnus Hvidevold <hvidevold@gmail.com>
2020-01-20 20:24:27 +0100

    extract data sets to separate files

--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ const main = function() {
   const seedrandom = require('seedrandom');
   const unitShader = require('./unitShader.js');
   const bargeJSON = require('./barge.js').bargeJSON;
+  const uniqueTerrain = require('./uniqueTerrain.js').uniqueTerrain;
+  const sets = require('./terrainSets.js').terrainSets;
+  const heightMap = require('./heightMap.js').heightMap;
 
   // Global PRNG: set Math.random.
   seedrandom('hello.', { global: true });
@@ -76,241 +79,7 @@ const main = function() {
 
   const validTerrainX = [];
 
-  const uniqueTerrain = {
-    "2X2": "dungeon/floor/acidic_floor_0",
-    "2X3": "dungeon/floor/acidic_floor_1",
-    "2X6": "dungeon/floor/black_cobalt_1",
-    "2X7": "dungeon/floor/black_cobalt_10",
-    "2X8": "dungeon/floor/black_cobalt_11",
-    "2X9": "dungeon/floor/black_cobalt_12",
-    "2X18": "dungeon/floor/bog_green_0_new",
-    "2X19": "dungeon/floor/bog_green_0_old",
-    "2X20": "dungeon/floor/bog_green_1_new",
-    "2X21": "dungeon/floor/bog_green_1_old",
-    "2X26": "dungeon/floor/cage_0",
-    "2X27": "dungeon/floor/cage_1",
-    "2X32": "dungeon/floor/cobble_blood_10_new",
-    "2X33": "dungeon/floor/cobble_blood_10_old",
-    "2X34": "dungeon/floor/cobble_blood_11_new",
-    "2X35": "dungeon/floor/cobble_blood_11_old",
-    "2X36": "dungeon/floor/cobble_blood_12_new",
-    "2X37": "dungeon/floor/cobble_blood_12_old",
-    "2X38": "dungeon/floor/cobble_blood_1_new",
-    "2X39": "dungeon/floor/cobble_blood_1_old",
-    "2X56": "dungeon/floor/crypt_10",
-    "2X57": "dungeon/floor/crypt_11",
-    "2X58": "dungeon/floor/crypt_domino_1a",
-    "2X59": "dungeon/floor/crypt_domino_1b",
-    "3X4": "dungeon/floor/crystal_floor_0",
-    "3X5": "dungeon/floor/crystal_floor_1",
-    "3X10": "dungeon/floor/demonic_red_1",
-    "3X19": "dungeon/floor/dirt_0_new",
-    "3X20": "dungeon/floor/dirt_0_old",
-    "3X21": "dungeon/floor/dirt_1_new",
-    "3X22": "dungeon/floor/dirt_1_old",
-    "3X27": "dungeon/floor/dirt_full_new",
-    "3X28": "dungeon/floor/dirt_full_old",
-    "3X43": "dungeon/floor/etched_0",
-    "3X44": "dungeon/floor/etched_1",
-    "3X49": "dungeon/floor/floor_nerves_0",
-    "3X50": "dungeon/floor/floor_nerves_1_new",
-    "3X51": "dungeon/floor/floor_nerves_1_old",
-    "3X61": "dungeon/floor/floor_sand_rock_0",
-    "3X62": "dungeon/floor/floor_sand_rock_1",
-    "4X1": "dungeon/floor/floor_sand_stone_0",
-    "4X2": "dungeon/floor/floor_sand_stone_1",
-    "4X9": "dungeon/floor/floor_vines_0_new",
-    "4X10": "dungeon/floor/floor_vines_0_old",
-    "4X11": "dungeon/floor/floor_vines_1_new",
-    "4X12": "dungeon/floor/floor_vines_1_old",
-    "4X23": "dungeon/floor/frozen_0",
-    "4X24": "dungeon/floor/frozen_1",
-    "4X25": "dungeon/floor/frozen_10",
-    "4X26": "dungeon/floor/frozen_11",
-    "4X27": "dungeon/floor/frozen_12",
-    "4X36": "dungeon/floor/green_bones_1",
-    "4X37": "dungeon/floor/green_bones_10",
-    "4X38": "dungeon/floor/green_bones_11",
-    "4X39": "dungeon/floor/green_bones_12",
-    "4X48": "dungeon/floor/grey_dirt_0_new",
-    "4X49": "dungeon/floor/grey_dirt_0_old",
-    "4X50": "dungeon/floor/grey_dirt_1_new",
-    "4X51": "dungeon/floor/grey_dirt_1_old",
-    "5X0": "dungeon/floor/grey_dirt_b_0",
-    "5X1": "dungeon/floor/grey_dirt_b_1",
-    "5X8": "dungeon/floor/hive_0",
-    "5X9": "dungeon/floor/hive_1",
-    "5X12": "dungeon/floor/ice_0_new",
-    "5X13": "dungeon/floor/ice_0_old",
-    "5X14": "dungeon/floor/ice_1_new",
-    "5X15": "dungeon/floor/ice_1_old",
-    "5X20": "dungeon/floor/infernal_1",
-    "5X21": "dungeon/floor/infernal_10",
-    "5X22": "dungeon/floor/infernal_11",
-    "5X23": "dungeon/floor/infernal_12",
-    "5X24": "dungeon/floor/infernal_13",
-    "5X25": "dungeon/floor/infernal_14",
-    "5X26": "dungeon/floor/infernal_15",
-    "5X35": "dungeon/floor/infernal_blank",
-    "5X36": "dungeon/floor/labyrinth_0",
-    "5X37": "dungeon/floor/labyrinth_1",
-    "5X40": "dungeon/floor/lair0b",
-    "5X41": "dungeon/floor/lair1b",
-    "5X42": "dungeon/floor/lair2b",
-    "5X43": "dungeon/floor/lair3b",
-    "5X44": "dungeon/floor/lair4b",
-    "5X45": "dungeon/floor/lair5b",
-    "5X46": "dungeon/floor/lair6b",
-    "5X47": "dungeon/floor/lair7b",
-    "5X48": "dungeon/floor/lair_0_new",
-    "5X49": "dungeon/floor/lair_0_old",
-    "5X50": "dungeon/floor/lair_1_new",
-    "5X51": "dungeon/floor/lair_1_old",
-    "5X60": "dungeon/floor/lava_0",
-    "5X61": "dungeon/floor/lava_1",
-    "6X0": "dungeon/floor/lava_old",
-    "6X1": "dungeon/floor/limestone_0",
-    "6X2": "dungeon/floor/limestone_1",
-    "6X11": "dungeon/floor/marble_floor_1",
-    "6X19": "dungeon/floor/mesh_1_new",
-    "6X20": "dungeon/floor/mesh_0_old",
-    "6X25": "dungeon/floor/mosaic_0",
-    "6X26": "dungeon/floor/mosaic_1",
-    "6X27": "dungeon/floor/mosaic_10",
-    "6X28": "dungeon/floor/mosaic_11",
-    "6X29": "dungeon/floor/mosaic_12",
-    "6X30": "dungeon/floor/mosaic_13",
-    "6X31": "dungeon/floor/mosaic_14",
-    "6X32": "dungeon/floor/mosaic_15",
-    "6X41": "dungeon/floor/moss_0",
-    "6X42": "dungeon/floor/moss_1",
-    "6X45": "dungeon/floor/mud_0",
-    "6X46": "dungeon/floor/mud_1",
-    "6X49": "dungeon/floor/orc_0",
-    "6X50": "dungeon/floor/orc_1",
-    "6X57": "dungeon/floor/pebble_brown_0_new",
-    "6X58": "dungeon/floor/pebble_brown_0_old",
-    "6X59": "dungeon/floor/pebble_brown_1_new",
-    "6X60": "dungeon/floor/pebble_brown_1_old",
-    "7X12": "dungeon/floor/pedestal_full",
-    "7X20": "dungeon/floor/rect_gray_0_new",
-    "7X21": "dungeon/floor/rect_gray_0_old",
-    "7X22": "dungeon/floor/rect_gray_1_new",
-    "7X23": "dungeon/floor/rect_gray_1_old",
-    "7X28": "dungeon/floor/rough_red_0",
-    "7X29": "dungeon/floor/rough_red_1",
-    "7X32": "dungeon/floor/sandstone_floor_0",
-    "7X33": "dungeon/floor/sandstone_floor_1",
-    "7X42": "dungeon/floor/sand_1",
-    "8X25": "dungeon/floor/snake-a_0",
-    "8X26": "dungeon/floor/snake-a_1",
-    "8X29": "dungeon/floor/snake-c_0",
-    "8X30": "dungeon/floor/snake-c_1",
-    "8X33": "dungeon/floor/snake-d_0",
-    "8X34": "dungeon/floor/snake-d_1",
-    "8X37": "dungeon/floor/snake_0",
-    "8X38": "dungeon/floor/snake_1",
-    "8X42": "dungeon/floor/swamp_0_new",
-    "8X43": "dungeon/floor/swamp_1_new",
-    "8X44": "dungeon/floor/swamp_1_old",
-    "8X49": "dungeon/floor/tomb_0_new",
-    "8X50": "dungeon/floor/tomb_0_old",
-    "8X52": "dungeon/floor/tomb_1_new",
-    "8X57": "dungeon/floor/tutorial_pad",
-    "8X58": "dungeon/floor/volcanic_floor_0",
-    "8X59": "dungeon/floor/volcanic_floor_1",
-    "9X1": "dungeon/floor/white_marble_0",
-    "9X2": "dungeon/floor/white_marble_1",
-    "9X11": "dungeon/floor/grass/grass0-dirt-mix_1",
-    "9X16": "dungeon/floor/grass/grass_1_new",
-    "9X17": "dungeon/floor/grass/grass_1_old",
-    "9X22": "dungeon/floor/grass/grass_flowers_blue_1_new",
-    "9X23": "dungeon/floor/grass/grass_flowers_blue_1_old",
-    "9X27": "dungeon/floor/grass/grass_0_old",
-    "9X28": "dungeon/floor/grass/grass_flowers_red_1_new",
-    "9X34": "dungeon/floor/grass/grass_flowers_yellow_1_new",
-    "9X35": "dungeon/floor/grass/grass_flowers_yellow_1_old",
-    "9X40": "dungeon/floor/grass/grass_full_new",
-    "9X41": "dungeon/floor/grass/grass_full_old",
-    "21X19": "dungeon/water/deep_water",
-    "21X21": "dungeon/water/deep_water_murky",
-    "21X47": "dungeon/water/grey_dirt_bl",
-    "21X61": "dungeon/water/liquefaction_1",
-    "21X63": "dungeon/water/open_sea",
-    "22X9": "dungeon/water/shallow_water",
-    "22X11": "dungeon/water/shallow_water_disturbance",
-    "22X13": "dungeon/water/shallow_water_murky",
-    "22X15": "dungeon/water/shallow_water_murky_disturbance",
-    "22X33": "dungeon/water/shoals_deep_water_0",
-    "22X34": "dungeon/water/shoals_deep_water_10",
-    "22X35": "dungeon/water/shoals_deep_water_11",
-    "22X36": "dungeon/water/shoals_deep_water_1_new",
-    "22X37": "dungeon/water/shoals_deep_water_1_old",
-    "22X38": "dungeon/water/shoals_deep_water_1_shape",
-    "22X54": "dungeon/water/shoals_shallow_water_0",
-    "22X55": "dungeon/water/shoals_shallow_water_10",
-    "22X56": "dungeon/water/shoals_shallow_water_11",
-    "22X57": "dungeon/water/shoals_shallow_water_1_new",
-    "22X58": "dungeon/water/shoals_shallow_water_1_old",
-    "23X6": "dungeon/water/shoals_shallow_water_disturbance_1_new",
-    "23X7": "dungeon/water/shoals_shallow_water_disturbance_1_old",
-  };
 
-  const sets = [
-    ["5X23","22X34","6X27","6X0","7X32","8X34"],      // sandstone
-    ["2X20","5X41","4X12","5X22","2X56","5X46"],      // dark
-    ["8X58","4X24","2X3","3X28","6X0","4X26"],        // acidic 2
-    ["5X15","22X55","7X32","4X38","4X50","6X29"],     // arctic
-    ["5X61","6X26","22X15","2X37","9X40","8X34"],     // green
-    ["6X11","3X19","2X36","6X29","8X49","21X21"],     // cobble
-    ["21X47","6X60","22X58","2X8","6X31","22X37"],    // oceanic
-    ["22X15","6X1","2X37","2X38","5X40","4X39"],      // green desert
-    ["22X9","9X23","4X9","9X1","9X2","9X35"],         // marble
-    ["23X7","7X42","7X32","3X62","8X37","5X35"],      // desert
-    ["21X47","9X16","9X11","8X52","3X44","2X2"],      // acidic
-    ["22X35","8X25","2X57","2X7","4X12","6X41"],      // darkgreen
-    ["21X21","8X25","4X11","6X45","6X41","2X26"],     // lightgreen
-    ["22X37","2X9","3X61","5X47","5X37","6X19"],      // contrast
-    ["23X7","4X23","3X62","8X43","6X2","2X27"],       // limestone
-    ["22X37","3X20","5X40","9X23","5X13","5X48"],     // grass+dirt
-    ["23X2","9X11","9X14","9X15","7X21","14X41"]      // standard
-  ];
-
-  const heightMap =
-    [
-[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-[35,51,56,61,74,83,90,90,90,90,90,99,111,116,116,116,116,116,116,116,116,116,114,104,80,0,0,0,0,0,0,0],
-[87,103,108,111,111,113,113,113,113,113,113,115,121,123,123,123,123,123,123,123,123,123,123,120,89,0,0,0,0,0,0,0],
-[98,108,114,115,115,113,113,113,113,113,113,115,115,116,116,116,116,116,116,116,116,116,116,111,89,0,0,0,0,0,0,0],
-[180,169,161,150,157,166,176,176,176,176,176,185,194,200,200,200,200,200,200,200,200,200,194,173,136,0,0,0,0,0,0,0],
-[244,253,253,252,252,252,252,252,252,252,252,252,248,248,248,249,251,251,251,248,248,248,248,240,188,0,0,0,0,0,0,0],
-[230,244,245,244,244,244,244,244,244,244,244,244,248,248,248,249,252,254,251,248,248,248,248,240,188,0,0,0,0,0,0,0],
-[120,138,138,136,136,136,136,136,136,136,136,158,173,184,189,201,230,230,225,200,200,194,189,169,136,0,0,0,0,0,0,0],
-[80,110,117,118,117,117,117,117,117,117,117,117,117,117,117,116,116,116,116,116,116,116,117,111,89,0,0,0,0,0,0,0],
-[80,117,122,122,121,121,121,121,121,121,121,121,121,121,121,123,123,123,123,123,123,123,121,120,89,0,0,0,0,0,0,0],
-[71,100,110,111,110,110,110,110,110,110,110,110,110,110,112,114,116,116,116,116,116,114,112,100,80,0,0,0,0,0,0,0],
-[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-[0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-[0,0,0,0,0,0,89,111,119,117,117,117,116,116,114,112,112,114,116,116,116,116,116,116,116,116,116,116,116,116,108,89],
-[0,0,0,0,0,0,147,130,134,128,128,128,123,123,123,128,128,123,123,123,123,123,123,123,123,123,123,123,123,123,123,120],
-[0,0,0,0,0,0,187,195,172,172,163,158,153,148,148,144,144,148,148,148,148,148,148,148,148,148,148,148,148,148,166,188],
-[0,0,0,0,0,0,218,254,255,254,254,254,254,254,245,237,237,246,255,255,255,255,254,254,255,255,255,255,255,255,255,250],
-[0,0,0,0,0,0,180,219,232,226,217,217,212,207,200,200,200,220,224,235,227,227,216,225,231,237,240,243,245,244,240,218],
-[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-[76,96,106,109,111,109,107,105,105,105,105,102,97,91,91,91,91,91,91,98,98,104,105,105,91,60,0,0,0,0,0,0],
-[91,112,116,118,118,118,116,116,116,116,116,116,114,114,114,114,114,114,114,116,117,116,117,117,112,85,0,0,0,0,0,0],
-[86,106,111,111,111,111,112,112,112,112,112,112,114,114,114,114,114,114,114,116,117,116,116,116,112,85,0,0,0,0,0,0],
-[166,197,212,222,230,222,215,208,208,208,201,183,176,168,168,168,176,176,174,160,166,168,183,183,188,174,0,0,0,0,0,0],
-[202,244,252,252,252,252,252,252,252,252,252,248,251,253,251,250,253,253,250,252,252,255,255,255,255,188,0,0,0,0,0,0],
-[190,230,241,244,244,244,244,244,244,244,244,248,252,253,251,250,251,252,250,244,244,241,241,241,225,188,0,0,0,0,0,0]
-];
 
   /*
   for (let i = 11; i < 39; i++) {
@@ -410,11 +179,13 @@ const main = function() {
     //data[stride + 1] = (Math.floor(500.0 / 32.0) - 1.0) * yf;
     data[stride + 2] = height;
     // Tile brightness
-    if (tile == validTerrain[0]) {
+    /*
+    if (i == 0) {
       data[stride + 3] = 1.0;
     } else {
-      data[stride + 3] = 1.0;
+      data[stride + 3] = 0.0;
     }
+    */
   }
 
   for (let i = 0; i < dataTextureSizeUnits; i++) {
@@ -606,8 +377,13 @@ const main = function() {
 
       vec2 indexOffset = vec2(0.5) / segmentsRes;
 
+      vec4 index = texture2D(mapIndex, uvIndex + indexOffset);
+      //index = vec2(0.0, 14.0) * tileSizeOnTexture;
+      index.y = 1.0 - index.y;
+
       vec2 uv = fract(vuv * segments);
       uv = vUvOffset;
+      //uv = fract(uv + index.w * vec2(time));
       // Fix vertical slopes
       // uv.x = fract(uv + vPosition.y / 10.0 * segments).x;
 
@@ -615,12 +391,9 @@ const main = function() {
 
       vec2 uvRel = uvRel0to1 * tileSizeOnTexture;
 
-      vec4 index = texture2D(mapIndex, uvIndex + indexOffset);
-      //index = vec2(0.0, 14.0) * tileSizeOnTexture;
-      index.y = 1.0 - index.y;
 
       vec4 color = getTilePixel(index.xy, uvRel);
-      color.rgb *= index.w;
+      //color.rgb *= index.w;
 
 
       const float lightSpeed = 2.0;
